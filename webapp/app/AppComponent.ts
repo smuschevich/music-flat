04d9b8bdaa6161e86747ab76d063d8c47c6f4559
Andrey Suschevich <andrej.suschevich@intexsoft.by>
2016-07-20 14:42:23 +0300

    Fixed redirect problem with OAuth of Facebook

--- webapp/app/AppComponent.ts
+++ webapp/app/AppComponent.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {RouteConfig} from '@angular/router-deprecated';
+import {RouteConfig, Router} from '@angular/router-deprecated';
 import {HomeComponent} from './home/HomeComponent';
 import {LoggedInRouterOutlet} from './LoggedInRouterOutlet';
 import {DashboardComponent} from './dashboard/DashboardComponent';
+import {Location} from '@angular/common';
 
 @Component({
 	selector: 'music-flat-app',
@@ -11,7 +12,17 @@ import {DashboardComponent} from './dashboard/DashboardComponent';
 })
 @RouteConfig([
 	{path: '/home', name: 'Home', component: HomeComponent, useAsDefault: true},
+	// {regex: '_=_',  name: 'OAuthRedirectUri', serializer: (p) => console.log(p), component: HomeComponent},
 	{path: '/dashboard', name: 'Dashboard', component: DashboardComponent}
 ])
 export class AppComponent {
+	constructor(private location: Location, private router: Router) {
+		this.verifyOauthUrlHash();
+	}
+
+	private verifyOauthUrlHash() {
+		if (this.location.path(true) === '_=_') {
+			this.router.navigateByUrl('/');
+		}
+	}
 }
